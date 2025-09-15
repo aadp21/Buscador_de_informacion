fd123d5bacc1119628a656018e78d42ff1ccd32e
aadp21 <aa.delgado.pena@gmail.com>
2025-09-15 12:39:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import os
+import json
 from fastapi import FastAPI
 from conector_sheets import leer_hoja
 
 SHEET_ID = "18e8Bfx5U1XLar7DOQ7PSVO5nQzluqKBHaxSOfRcreRI"
 
-# Crear la app FastAPI
 app = FastAPI()
 
 @app.get("/")
 def raiz():
     return {"mensaje": "Hola, FastAPI en la nube 🚀"}
 
-@app.get("/pop")
-def obtener_pop():
-    df = leer_hoja(SHEET_ID, "Bases POP")
-    return df.head(10).to_dict(orient="records")  # primeros 10 registros como JSON
+@app.get("/test-credentials")
+def test_credentials():
+    creds_json = os.getenv("GOOGLE_CREDENTIALS")
+    if creds_json:
+        try:
+            creds_dict = json.loads(creds_json)
+            return {"status": "OK ✅", "project_id": creds_dict.get("project_id", "No encontrado")}
+        except Exception as e:
+            return {"status": "ERROR ❌", "detalle": str(e)}
+    else:
+        return {"status": "NO ENCONTRADO ❌"}
